a81ef104990af0828ab567241e23507b1bf94352
Dr Nick Martin <en51nm@gmail.com>
2017-02-26 22:55:08 +0000

    updated canvas size

--- public/javascripts/globals.js
+++ public/javascripts/globals.js
@@ -6,8 +6,8 @@ class Globals {
     this.dtms = 1000/this.FPS;
     this.g = 9.81;
     this.thrust = 50;
-    this.canvas_width = 1680;
-    this.canvas_height = 1050;
+    this.canvas_width = 1200;
+    this.canvas_height = 600;
     this.num_enemies = 15;
   }
 }
